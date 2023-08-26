1fda2d673c00232c8d280736fdf5945e573ac8b2
Zeu Capua <zeucapua@gmail.com>
2023-08-26 14:47:45 -0700

    added astro vercel adapter, partykit deploy in build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  adapter: vercel()
 });
